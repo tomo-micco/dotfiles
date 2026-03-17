d9a9c90b2860cd2041730468e1757846b39912a8
tomo-micco <75841218+tomo-micco@users.noreply.github.com>
2026-03-18 08:59:55 +0900

    telescopeのテーマが反映されるように修正

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,30 @@ return {
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         },
         config = function()
+            local telescope = require('telescope')
             local builtin = require('telescope.builtin')
+
+            telescope.setup {
+                defaults = {
+                    theme = "ivy",
+                },
+                pickers = {
+                    find_files = {
+                        theme = "dropdown",
+                    }
+                },
+                extensions = {
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    }
+                }
+            }
+
+            telescope.load_extension('fzf')
+
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
